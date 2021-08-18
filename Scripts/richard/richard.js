ef96fd3022eedb1ed3e03ec76d4710307cbeb960
Joyce Lusia <joycep.lusia@gmail.com>
2021-08-17 23:09:57 -0300

    Ultima atualização do dia

--- Scripts/richard/richard.js
+++ Scripts/richard/richard.js
@@ -4,12 +4,11 @@ var texto = document.getElementById("frase");
  
 
  function n1(){
-   console.log("Funcionando");
-    var acao1 = prompt("Escolha por onde ir: \n1- Deixar a encomenda na porta\n2- Deixar a encomenda na biblioteca\n3- Deixar a encomenda na sala ")
+    var acao1 = prompt("Escolha por onde ir: \n1- Deixar a encomenda na porta\n2- Deixar a encomenda na biblioteca")
    
     
     if(acao1 == 1){
-     texto.innerHTML="<p>Muito obrigada, Richard! Bom trabalho. Da próxima vez, você poderia entrar pra tomar um café, que tal? </p>" 
+     texto.innerHTML="Muito obrigada, Richard! Bom trabalho. Da próxima vez, você poderia entrar pra tomar um café, que tal? " 
      buttom.onclick = location.href("../HTML/game.html");
       
       
@@ -17,16 +16,61 @@ var texto = document.getElementById("frase");
     }
     else if (acao1 == 2){
       texto.innerHTML="Parece que faz muito tempo que ninguém lê um livro por aqui, não é? Não tem uma superfície sem poeira para que você possa deixar esse pacote. Talvez ali na mesa, junto com as outras caixas? É impressão minha ou essa encomenda se mexeu? "
+      return n2Biblioteca();
      
       
     }
 
-    else if(acao1==3){
-      texto.innerHTML="Esse lugar estava realmente precisando de uma visita, por que você não senta e descansa um pouquinho? Esse sofá parece muito aconchega"
-    }
    else{
      texto.innerHTML= "Digite escolha válida"
   
    }
 
   }
+
+  function n2Biblioteca(){
+    var acao1 = prompt("O que você quer fazer?\n 1 - Abrir o pacote\n 2 - Deixar a encomenda na mesa e ir embora")
+
+    if(acao1 ==1){
+      texto.innerHTML= "Toc! Toc! Você pode achar que é só uma mão decepada se mexendo, mas esse é o Larry. Eu estava esperando ele há muito tempo. Parece que ele gostou de você… "
+      return gameOverBiblioteca();
+    }
+
+    else if(acao1 ==2){
+      texto.innerHTML = "Mas já? Tem certeza que não quer dar uma volta antes de seguir com o seu trabalho?"
+      return decisao();
+    }
+    else{
+      texto.innerHTML= "Digite opção válida"
+    }
+  }
+
+  function decisao(){
+    var acao1 = prompt("1- Ir para sala \n 2-Ir embora")
+
+     if(acao1==1){
+      texto.innerHTML="Uma pausa me faria bem mesmo, o expediente já está terminando, ninguém vai se importar…"
+      return gameOverPausa();
+    }
+
+    else if(acao1==2){
+      texto.innerHTML=" Seria bom deixar isso aqui e ir logo descansar na minha própria casa…"
+      return gameOverCasa();
+    }
+
+    else{
+      texto.innerHTML= "Digite opção válida"
+    }
+  }
+
+  function gameOverPausa(){
+
+    texto.innerHTML = "Nada melhor que nosso próprio lar, não é? Mas eu prometo que aqui você pode se sentir em casa para sempre… quer dizer, sempre que quiser…"
+  }
+  
+  
+  function gameOverCasa(){
+  
+    texto.inner =   "Esse sofá é muito confortável! Você olha ao redor e percebe como é estranho um móvel tão novo no meio de tantos antigos… Parece que ele acabou de sair da loja. Mas… O que é isso, Richard? Seu cabelo não era branco quando você entrou aqui… E sua pele está… enrugando?"
+  }
+
